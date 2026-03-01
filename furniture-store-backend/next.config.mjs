73fab9eb33e7093fb6dffa2281b8d9ac85107032
Rahul Swarup <rahulsw24@gmail.com>
2026-03-01 11:12:25 +0530

    fix backend test

--- furniture-store-backend/next.config.mjs
+++ furniture-store-backend/next.config.mjs
@@ -1,16 +1,19 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import path from 'path'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
+  webpack: (config) => {
+    // ⭐ ADD THIS
+    config.resolve.alias['@payload-config'] = path.resolve('./src/payload.config.ts')
+
+    config.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
 
-    return webpackConfig
+    return config
   },
 }
 
